3cd935bb33ca6fca51d7dfe8d682acbfdff98247
nahuel.alvarez <nahuel.alvarez@bbva.com>
2018-02-21 13:11:03 -0300

    Se implementa el endpoint /biblioteca/libros.

--- biblioserver.py
+++ biblioserver.py
@@ -11,11 +11,44 @@ app = Flask(__name__)
 
 @app.route('/biblioteca/libros', methods = ['GET', 'POST'])
 def api_libros():
-    return 'Lista de libros\n'
+    if request.method == 'GET':
+        resp = Response(json.dumps(libros), status=200, mimetype='application/json')
+    else:
+        if request.headers['Content-Type'] == 'application/json':
+            if "id" in request.json.keys():
+                data = {
+                    "error"  : "No debe especificar 'id'."
+                }
+                resp = Response(json.dumps(data), status=400, mimetype='application/json')
+            elif "titulo" not in request.json.keys():
+                data = {
+                    "error"  : "Debe especificar 'titulo'."
+                }
+                resp = Response(json.dumps(data), status=400, mimetype='application/json')
+            elif "publicado" not in request.json.keys():
+                data = {
+                    "error"  : "Debe especificar 'publicado'."
+                }
+                resp = Response(json.dumps(data), status=400, mimetype='application/json')
+            elif type(request.json["publicado"]) is not int:
+                data = {
+                    "error"  : "'publicado' debe ser númerico."
+                }
+                respResponse(json.dumps(data), status=400, mimetype='application/json')
+            else:
+                libro = request.json
+                libro["id"] = len(libros) + 1
+                libros.append(libro)
+                resp = Response(None, status=201, mimetype='application/json')
+                resp.headers['Location'] = 'http://%s/biblioteca/libros/%s' % (request.headers.environ['HTTP_HOST'],len(libros))
+        else:
+            resp = Response(None, status=415, mimetype='application/json')
+    return resp
 
 @app.route('/biblioteca/libros/<id>', methods = ['GET', 'PUT', 'DELETE']) #, 'PATCH'
 def api_librosId(id):
     return 'Detalle del libro con Id=' + id + '\n'
 
 if __name__ == '__main__':
+    libros = []
     app.run(host= '0.0.0.0')
--- probar.sh
+++ probar.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
-# Recuperar la lista de libros
-curl http://localhost:5000/biblioteca/libros
-# Recuperar el libro 1
-curl http://localhost:5000/biblioteca/libros/1
+SERVIDOR=localhost
+PUERTO=5000
+echo "== PRUEBAS GENERALES DEL SERVICIO ================================="
+echo ""
+echo "-- Recuperar la lista de libros. ----------------------------------"
+echo ""
+curl -X GET http://$SERVIDOR:$PUERTO/biblioteca/libros
+echo ""
+echo "-- Recuperar el libro 1. ------------------------------------------"
+curl -X GET http://$SERVIDOR:$PUERTO/biblioteca/libros/1
+echo ""
+echo "== PRUEBAS SOBRE EL END-POINT /biblioteca/libros =================="
+echo ""
+echo "-- Recuperar la lista de libros. ----------------------------------"
+echo ""
+curl -v -X GET http://$SERVIDOR:$PUERTO/biblioteca/libros
+echo ""
+echo "-- Crear un libro 1. ----------------------------------------------"
+echo ""
+curl -v -X POST http://$SERVIDOR:$PUERTO/biblioteca/libros \
+     -H 'Content-Type: application/json' \
+     -d '{"titulo": "Organizaciones Exponenciales", "publicado":2014 }'
+echo ""
+echo "-- Crear un libro 2. ----------------------------------------------"
+echo ""
+curl -v -X POST http://$SERVIDOR:$PUERTO/biblioteca/libros \
+     -H 'Content-Type: application/json' \
+     -d '{"titulo": "Bash Guide for Beginners", "publicado":2004 }'
+echo ""
+echo "-- Recuperar la lista de libros. ----------------------------------"
+echo ""
+curl -X GET http://$SERVIDOR:$PUERTO/biblioteca/libros
+echo ""
+echo "-- Intento erroneo de eliminar un libro sin identificarlo. --------"
+echo ""
+curl -X DELETE http://$SERVIDOR:$PUERTO/biblioteca/libros
+echo ""
+echo "-- Intento erroneo de actualizar un libro sin identificarlo. ------"
+echo ""
+curl -X PUT http://$SERVIDOR:$PUERTO/biblioteca/libros
+echo ""
+echo "** FIN DE LAS PRUEBAS *********************************************"
